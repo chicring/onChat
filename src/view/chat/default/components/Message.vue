8fb4fc39ca0d5bc2e538351ea07af74c17eb8eb0
JHong <3081044735@qq.com>
2024-05-08 20:47:46 +0800

    feat: 消息复制按钮

--- src/view/chat/default/components/Message.vue
+++ src/view/chat/default/components/Message.vue
@@ -23,43 +23,116 @@ function copyTextToClipboard() {
 
 <template>
 
-  <div v-if="props.message.role !== 'user'" style="display: flex; justify-content: start" class="mb-1">
-    <BrandOpenaiIcon v-if="props.message.role !== 'user'"></BrandOpenaiIcon>
-    <span class="small-font align-self-end ml-1">{{formatDate(message.date)}}</span>
+  <div v-if="props.message.role !== 'user'">
+    <div style="display: flex; justify-content: start" class="mb-1">
+      <BrandOpenaiIcon v-if="props.message.role !== 'user'"></BrandOpenaiIcon>
+      <span class="small-font align-self-end ml-1">{{formatDate(message.date)}}</span>
+    </div>
+
+    <v-col style="display: flex; justify-content: start" class="pa-0 ma-0">
+      <v-card elevation="0" rounded="md" class="ml-1" style="max-width: 93%">
+        <template #text>
+          <MarkdownText :text="props.message.content"></MarkdownText>
+        </template>
+      </v-card>
+
+      <div class="d-flex opacity-60 flex-column justify-end ml-1 mb-1">
+
+        <v-btn size="30" variant="text" icon>
+          <ArrowBackUpIcon size="20"></ArrowBackUpIcon>
+          <v-tooltip
+              activator="parent"
+              location="top"
+          >重试</v-tooltip>
+        </v-btn>
+
+        <v-btn icon="true" variant="text" @click.stop.prevent size="x-small">
+          <DotsVerticalIcon size="20"></DotsVerticalIcon>
+          <v-menu
+              activator="parent"
+          >
+            <v-card>
+              <v-btn  variant="text"   @click="copyTextToClipboard">
+                <CopyIcon size="20"></CopyIcon>
+                复制
+              </v-btn>
+            </v-card>
+          </v-menu>
+        </v-btn>
+      </div>
+    </v-col>
+
   </div>
 
-  <div v-else style="display: flex; justify-content: end" class="mb-1">
-    <span class="small-font align-self-end mr-1">{{formatDate(message.date)}}</span>
-    <UserCircleIcon></UserCircleIcon>
+  <div v-else>
+    <div style="display: flex; justify-content: end" class="mb-1">
+      <span class="small-font align-self-end mr-1">{{formatDate(message.date)}}</span>
+      <UserCircleIcon></UserCircleIcon>
+    </div>
+
+    <v-col style=" display: flex; justify-content: end" class="pa-0 ma-0">
+
+      <div class="align-self-end opacity-60 mb-1">
+        <v-btn icon="true" variant="text" @click.stop.prevent size="x-small">
+          <DotsVerticalIcon size="20"></DotsVerticalIcon>
+          <v-menu
+              activator="parent"
+          >
+            <v-card>
+              <v-btn  variant="text"   @click="copyTextToClipboard">
+                <CopyIcon size="20"></CopyIcon>
+                复制
+              </v-btn>
+            </v-card>
+          </v-menu>
+        </v-btn>
+      </div>
+
+      <v-card elevation="0" rounded="md" class="ml-1" style="max-width: 93%">
+        <template #text>
+          <MarkdownText :text="props.message.content"></MarkdownText>
+        </template>
+      </v-card>
+    </v-col>
+
   </div>
 
-  <v-col :style="{ display: 'flex', 'justify-content': props.message.role === 'user' ? 'end' : 'start' }" class="pa-0 ma-0">
-    <v-card elevation="0" rounded="md" class="ml-1" style="max-width: 93%">
-      <template #text>
-        <MarkdownText :text="props.message.content"></MarkdownText>
-      </template>
 
-    </v-card>
-  </v-col>
-  <div class="mx-4 ga-2" v-if="props.message.role !== 'user'">
+<!--  <div v-if="props.message.role !== 'user'" style="display: flex; justify-content: start" class="mb-1">-->
+<!--    <BrandOpenaiIcon v-if="props.message.role !== 'user'"></BrandOpenaiIcon>-->
+<!--    <span class="small-font align-self-end ml-1">{{formatDate(message.date)}}</span>-->
+<!--  </div>-->
 
-    <v-btn size="30" variant="text" class="opacity-30" icon>
-      <ArrowBackUpIcon size="20"></ArrowBackUpIcon>
-      <v-tooltip
-          activator="parent"
-          location="top"
-      >重试</v-tooltip>
-    </v-btn>
+<!--  <div v-else style="display: flex; justify-content: end" class="mb-1">-->
+<!--    <span class="small-font align-self-end mr-1">{{formatDate(message.date)}}</span>-->
+<!--    <UserCircleIcon></UserCircleIcon>-->
+<!--  </div>-->
 
-    <v-btn size="30" variant="text" class="opacity-30" icon @click="copyTextToClipboard">
-      <CopyIcon size="20"></CopyIcon>
-      <v-tooltip
-          activator="parent"
-          location="top"
-      >复制</v-tooltip>
-    </v-btn>
+<!--  <v-col :style="{ display: 'flex', 'justify-content': props.message.role === 'user' ? 'end' : 'start' }" class="pa-0 ma-0">-->
+<!--    <v-card elevation="0" rounded="md" class="ml-1" style="max-width: 93%">-->
+<!--      <template #text>-->
+<!--        <MarkdownText :text="props.message.content"></MarkdownText>-->
+<!--      </template>-->
+<!--    </v-card>-->
+<!--  </v-col>-->
+<!--  <div class="mx-4 ga-2" v-if="props.message.role !== 'user'">-->
 
-  </div>
+<!--    <v-btn size="30" variant="text" class="opacity-30" icon>-->
+<!--      <ArrowBackUpIcon size="20"></ArrowBackUpIcon>-->
+<!--      <v-tooltip-->
+<!--          activator="parent"-->
+<!--          location="top"-->
+<!--      >重试</v-tooltip>-->
+<!--    </v-btn>-->
+
+<!--    <v-btn size="30" variant="text" class="opacity-30" icon @click="copyTextToClipboard">-->
+<!--      <CopyIcon size="20"></CopyIcon>-->
+<!--      <v-tooltip-->
+<!--          activator="parent"-->
+<!--          location="top"-->
+<!--      >复制</v-tooltip>-->
+<!--    </v-btn>-->
+<!--  </div>-->
 
 </template>
 
